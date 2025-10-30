8fc6723c0f45fe09bdad20e38c218ec0aa23d173
Prakhar Jain <prakharparag04@gmail.com>
2025-10-30 18:48:12 +0530

    fix: remove extra bracket

--- server.js
+++ server.js
@@ -45,8 +45,6 @@ io.on("connection", (socket) => {
   });
 });
 
-});
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
